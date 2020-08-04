8adde0457b2e0e53876e60c667e41e99f884718f
Jakub N <ktosspl@gmail.com>
2020-08-04 16:08:23 +0200

    small refactor

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,31 @@
-import React, {useState} from 'react'
+import React, {
+  useState
+} from 'react'
 import ReactMapGL from 'react-map-gl'
 
-export default function Map(){
-  const [viewport,setViewport] = useState({
-    latitude: 54.35 ,
-    longitude:18.6667,
-    width:'100vw',
-    height:'100vh',
-    zoom:8
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 54.35,
+    longitude: 18.6667,
+    width: '100vw',
+    height: '100vh',
+    zoom: 8
 
   })
-  return(
-    <div>
-      <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      
-      >
-        markery
-      </ReactMapGL>
-    </div>
+  return ( <
+    div >
+    <
+    ReactMapGL {
+      ...viewport
+    }
+    mapboxApiAccessToken = {
+      process.env.REACT_APP_MAPBOX_TOKEN
+    }
+    onViewportChange = {
+      nextViewport => setViewport(nextViewport)
+    } >
+    markery <
+    /ReactMapGL> <
+    /div>
   )
 }
