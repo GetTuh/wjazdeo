047fd5c7e760fd0867f9103cef8790a105139152
Jakub N <ktosspl@gmail.com>
2020-08-05 14:03:14 +0200

    Added marker onClick

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,31 +1,39 @@
-import React, {
-  useState
-} from 'react'
-import ReactMapGL from 'react-map-gl'
+import React, {useState} from 'react'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'
 
-export default function Map() {
-  const [viewport, setViewport] = useState({
-    latitude: 54.35,
-    longitude: 18.6667,
-    width: '100vw',
-    height: '100vh',
-    zoom: 8
+export default function Map(){
+  const [viewport,setViewport] = useState({
+    latitude: 54.35 ,
+    longitude:18.6667,
+    width:'100vw',
+    height:'100vh',
+    zoom:10
 
   })
-  return ( <
-    div >
-    <
-    ReactMapGL {
-      ...viewport
-    }
-    mapboxApiAccessToken = {
-      process.env.REACT_APP_MAPBOX_TOKEN
-    }
-    onViewportChange = {
-      nextViewport => setViewport(nextViewport)
-    } >
-    markery <
-    /ReactMapGL> <
-    /div>
+  const [clickedPoint,setClickedPoint] =useState(null);
+  return(
+    <div>
+      <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+      onClick={clickedPoint=>{
+        setClickedPoint(clickedPoint)
+      }}
+      
+      >
+        {clickedPoint?(
+          <Marker key= {Math.random()*1000}
+          latitude={clickedPoint.lngLat[1]}
+          longitude={clickedPoint.lngLat[0]}>
+          <div>
+            Klik 
+          </div>
+        </Marker>
+        ):null}
+        
+      </ReactMapGL>
+    </div>
   )
 }
