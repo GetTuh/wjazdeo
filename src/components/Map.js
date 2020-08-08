419d37a6715a87abe3882e54af2b2dcad6a74074
Jakub N <ktosspl@gmail.com>
2020-08-09 00:17:15 +0200

    proper naming

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,19 +2,6 @@ import React, {useState} from 'react'
 import ReactMapGL, {Marker, CanvasOverlay } from 'react-map-gl'
 import PolylineOverlay from './PolylineOverlay'
 
-async function returnRoute(pointFrom,pointTo) {
-  let line=null;
-  line = await fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pointFrom.lngLat[0]},${pointFrom.lngLat[1]};${pointTo.lngLat[0]},${pointTo.lngLat[1]}?geometries=geojson&access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`)
-          .then(response => response.json())
-          .then(data => {
-            return data.routes[0].geometry.coordinates
-          })
-  await console.log(line)
-  return await line
-}
-
-
-
 export default function Map(){
   const [viewport,setViewport] = useState({
     latitude: 54.35 ,
@@ -43,18 +30,17 @@ export default function Map(){
           latitude={clickedPointFrom.lngLat[1]}
           longitude={clickedPointFrom.lngLat[0]}>
           <div>
-            Klik 
+            Od 
           </div>
         </Marker>
         
         ):null}
         {clickedPointTo?(
-          <Marker key= "elo"
+          <Marker key= "to"
           latitude={clickedPointTo.lngLat[1]}
           longitude={clickedPointTo.lngLat[0]}>
           <div>
-            elo  {()=> {
-              return returnRoute(clickedPointFrom,clickedPointTo)}}
+            Do
           </div>
           
         </Marker>):null}
